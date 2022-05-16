b48f39b6390b37b9b91a0ff31f9250adb2a81530
Adriano <adriano_alves_2008@hotmail.com>
2022-05-16 14:42:27 -0300

    mostrando a tarefa selecionada

--- src/components/lista/item/index.tsx
+++ src/components/lista/item/index.tsx
@@ -15,7 +15,7 @@ export default function Item(
     selecionaTarefa
   }: Props){
   return(
-    <li className={style.item} onClick={()=> selecionaTarefa({
+    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} onClick={()=> selecionaTarefa({
       tarefa, 
       tempo, 
       selecionado, 
